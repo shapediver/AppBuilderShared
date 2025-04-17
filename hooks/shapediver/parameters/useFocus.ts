dd6614b9d1d965171acb9f0f577e8754bad9e03e
Dmitry Rumyantsev <lemtoup@gmail.com>
2025-04-17 12:13:52 +0200

    SS-7784: Add useFocus hook

---
+++ hooks/shapediver/parameters/useFocus.ts
@@ -0,0 +1,27 @@
+import {FocusEvent, useEffect, useState} from "react";
+export const useFocus = () => {
+	const [focusedElement, setFocusedElement] =
+		useState<HTMLInputElement | null>(null);
+	const onFocusHandler = (e: FocusEvent<HTMLInputElement>) => {
+		if (e.target) {
+			setFocusedElement(e.target as HTMLInputElement);
+		}
+	};
+	const restoreFocus = () => {
+		if (focusedElement) {
+			focusedElement.focus();
+		}
+	};
+	const onBlurHandler = () => {
+		setFocusedElement(null);
+	};
+
+	useEffect(() => {
+		// Очистка focusedElement при размонтировании компонента
+		return () => {
+			setFocusedElement(null);
+		};
+	}, []);
+
+	return {focusedElement, onFocusHandler, restoreFocus, onBlurHandler};
+};
