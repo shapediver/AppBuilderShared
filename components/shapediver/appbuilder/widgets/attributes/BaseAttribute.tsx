1d13da1c4617db17f87a565519c67d1ac49d9bc2
Michael Oppitz <michael@shapediver.com>
2025-05-16 14:14:47 +0200

    SS-8426: AV widget new

--- components/shapediver/appbuilder/widgets/attributes/BaseAttribute.tsx
+++ components/shapediver/appbuilder/widgets/attributes/BaseAttribute.tsx
@@ -1,50 +1,21 @@
-import Icon from "@AppBuilderShared/components/ui/Icon";
-import {IconTypeEnum} from "@AppBuilderShared/types/shapediver/icons";
-import {ActionIcon, Grid, Group, Paper, Stack, Text} from "@mantine/core";
+import {Grid, Group, Paper} from "@mantine/core";
 import React from "react";
 
 interface BaseAttributeProps {
 	name: string;
 	type: string;
-	removeAttribute: (name: string, type: string) => void;
-	changeOrder: (name: string, type: string, direction: "up" | "down") => void;
+	style?: React.CSSProperties;
 	children?: React.ReactNode;
 	options?: React.ReactNode;
 }
 
 export default function BaseAttribute(props: BaseAttributeProps) {
-	const {name, type, children, options, removeAttribute, changeOrder} = props;
+	const {style, children, options} = props;
 
 	return (
-		<Paper>
+		<Paper style={style}>
 			<Grid align="center">
-				<Grid.Col span="content">
-					<Stack>
-						<ActionIcon
-							variant="default"
-							size="xs"
-							onClick={() => changeOrder(name, type, "up")}
-						>
-							<Icon type={IconTypeEnum.ArrowUp} />
-						</ActionIcon>
-						<ActionIcon
-							variant="default"
-							size="xs"
-							onClick={() => changeOrder(name, type, "down")}
-						>
-							<Icon type={IconTypeEnum.ArrowDown} />
-						</ActionIcon>
-					</Stack>
-				</Grid.Col>
 				<Grid.Col span="auto">
-					<Group justify="space-between">
-						<Text>{name}</Text>
-						<Icon
-							type={IconTypeEnum.X}
-							onClick={() => removeAttribute(name, type)}
-						/>
-					</Group>
-
 					<Group grow wrap="nowrap">
 						{children}
 					</Group>
--- types/store/shapediverStoreViewport.ts
+++ types/store/shapediverStoreViewport.ts
@@ -1,5 +1,6 @@
 import {IEventTracking} from "@AppBuilderShared/types/eventTracking";
 import {ViewportCreateDto} from "@AppBuilderShared/types/shapediver/viewport";
+import {IAttributeVisualizationEngine} from "@shapediver/viewer.features.attribute-visualization";
 import {IViewportApi} from "@shapediver/viewer.viewport";
 
 export interface IShapeDiverStoreViewports {
@@ -21,6 +22,13 @@ export interface IShapeDiverStoreViewport {
 	viewports: IShapeDiverStoreViewports;
 
 	/**
+	 * Attribute visualization engines currently known by the store.
+	 */
+	attributeVisualizationEngines: {
+		[viewportId: string]: IAttributeVisualizationEngine;
+	};
+
+	/**
 	 * Create a viewport and add it to the store.
 	 * @param dto
 	 * @returns
@@ -37,4 +45,18 @@ export interface IShapeDiverStoreViewport {
 		viewportId: string,
 		callbacks?: IShapeDiverStoreViewportCallbacks,
 	) => Promise<void>;
+
+	/**
+	 * Create an attribute visualization engine for a viewport and add it to the store.
+	 * @param viewportId
+	 * @returns
+	 */
+	createAttributeVisualizationEngine: (
+		viewportId: string,
+	) => IAttributeVisualizationEngine | undefined;
+
+	/**
+	 * Close an attribute visualization engine and remove it from the store.
+	 */
+	closeAttributeVisualizationEngine: (viewportId: string) => void;
 }
