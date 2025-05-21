1d676afcf27ff7642edce50b6932f3a7e081de2a
Dzmitry Rumyantsau <LemToUp@users.noreply.github.com>
2025-05-21 10:17:30 +0300

    Task/ss 7960 fix breakpoint related shift (#108)
    SS-7960: Fix breakpoint related layout shift

--- hooks/ui/useIsLandscape.ts
+++ hooks/ui/useIsLandscape.ts
@@ -2,6 +2,11 @@ import {useMediaQuery} from "@mantine/hooks";
 
 /**
  * Hook for deciding whether the device is a mobile (layout changes).
- * @returns
+ * @returns boolean
  */
-export const useIsLandscape = () => useMediaQuery("(orientation: landscape)");
+export const useIsLandscape = () =>
+	useMediaQuery(
+		"(orientation: landscape)",
+		false, // Mobile first
+		{getInitialValueInEffect: false},
+	);
