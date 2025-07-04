c24f8e79ff08209d0fe68028ab6b1bf3eb151254
Alexander Schiftner <alex@shapediver.com>
2025-07-04 12:45:01 +0200

    SS-8827 hide history buttons and dot-menu for now

--- components/shapediver/viewport/ViewportHistoryButtons.tsx
+++ components/shapediver/viewport/ViewportHistoryButtons.tsx
@@ -8,10 +8,14 @@ import {useCreateModelState} from "@AppBuilderShared/hooks/shapediver/useCreateM
 import {useViewportHistory} from "@AppBuilderShared/hooks/shapediver/viewer/useViewportHistory";
 import {useShapeDiverStoreParameters} from "@AppBuilderShared/store/useShapeDiverStoreParameters";
 import {IconTypeEnum} from "@AppBuilderShared/types/shapediver/icons";
-import {ActionIcon, Box, MantineStyleProp, Menu} from "@mantine/core";
+import {ActionIcon, Box, MantineStyleProp, Menu, useProps} from "@mantine/core";
 import React, {useCallback, useContext, useMemo, useState} from "react";
 
-interface Props {
+/**
+ * TODO SS-8827 this component will be refactored, it's ok to rename the
+ * styling props and make sure they are future proof.
+ */
+interface StyleProps {
 	/**
 	 * Style properties for the container
 	 */
@@ -41,12 +45,31 @@ interface Props {
 	 */
 	iconStyle?: MantineStyleProp;
 	/**
+	 * enable/disable the "parameter history" buttons
+	 */
+	enableHistoryButtons?: boolean;
+	/**
+	 * enable/disable the "model state" buttons
+	 */
+	enableModelStateButtons?: boolean;
+	/**
+	 * enable/disable the "import/export JSON file" buttons
+	 */
+	enableImportExportButtons?: boolean;
+	/**
+	 * enable/disable the "reset parameter values" button
+	 */
+	enableResetButton?: boolean;
+}
+
+interface Props {
+	/**
 	 * Namespace of the session
 	 */
 	namespace: string;
 }
 
-const defaultProps: Required<Omit<Props, "namespace">> = {
+const defaultStyleProps: StyleProps = {
 	style: {display: "flex", gap: "0.25rem"},
 	size: 32,
 	color: "black",
@@ -54,6 +77,10 @@ const defaultProps: Required<Omit<Props, "namespace">> = {
 	variant: "subtle",
 	variantDisabled: "transparent",
 	iconStyle: {m: "0.188rem"},
+	enableHistoryButtons: false,
+	enableModelStateButtons: false,
+	enableImportExportButtons: false,
+	enableResetButton: false,
 };
 
 /**
@@ -61,7 +88,9 @@ const defaultProps: Required<Omit<Props, "namespace">> = {
  * Includes browser history navigation and parameter/model state management.
  * Buttons are disabled when there are pending parameter changes that need to be accepted or rejected.
  */
-export default function ViewportHistoryButtons(props: Props) {
+export default function ViewportHistoryButtons(props: Props & StyleProps) {
+	const {namespace, ...rest} = props;
+
 	const {
 		style,
 		size,
@@ -70,8 +99,16 @@ export default function ViewportHistoryButtons(props: Props) {
 		variant,
 		variantDisabled,
 		iconStyle,
-		namespace,
-	} = {...defaultProps, ...props};
+		enableHistoryButtons,
+		enableModelStateButtons,
+		enableImportExportButtons,
+		enableResetButton,
+	} = useProps("ViewportHistoryButtons", defaultStyleProps, rest);
+
+	const enableMenu =
+		enableResetButton ||
+		enableImportExportButtons ||
+		enableModelStateButtons;
 
 	const [isImportDialogOpen, setIsImportDialogOpen] = useState(false);
 
@@ -139,123 +176,150 @@ export default function ViewportHistoryButtons(props: Props) {
 
 	return (
 		<Box style={style}>
-			<TooltipWrapper
-				label={
-					hasPendingChanges
-						? "Accept or reject changes first"
-						: "Undo"
-				}
-			>
-				<ActionIcon
-					onClick={goBack}
-					disabled={!canGoBack || buttonsDisabled || executing}
-					size={size}
-					variant={
-						!canGoBack || buttonsDisabled
-							? variantDisabled
-							: variant
-					}
-					aria-label="Undo"
-					style={iconStyle}
-				>
-					<Icon
-						type={IconTypeEnum.ArrowBackUp}
-						color={
-							!canGoBack || buttonsDisabled
-								? colorDisabled
-								: color
+			{enableHistoryButtons && (
+				<>
+					<TooltipWrapper
+						label={
+							hasPendingChanges
+								? "Accept or reject changes first"
+								: "Undo"
 						}
-					/>
-				</ActionIcon>
-			</TooltipWrapper>
+					>
+						<ActionIcon
+							onClick={goBack}
+							disabled={
+								!canGoBack || buttonsDisabled || executing
+							}
+							size={size}
+							variant={
+								!canGoBack || buttonsDisabled
+									? variantDisabled
+									: variant
+							}
+							aria-label="Undo"
+							style={iconStyle}
+						>
+							<Icon
+								type={IconTypeEnum.ArrowBackUp}
+								color={
+									!canGoBack || buttonsDisabled
+										? colorDisabled
+										: color
+								}
+							/>
+						</ActionIcon>
+					</TooltipWrapper>
 
-			<TooltipWrapper
-				label={
-					hasPendingChanges
-						? "Accept or reject changes first"
-						: "Redo"
-				}
-			>
-				<ActionIcon
-					onClick={goForward}
-					disabled={!canGoForward || buttonsDisabled || executing}
-					size={size}
-					variant={
-						!canGoForward || buttonsDisabled
-							? variantDisabled
-							: variant
-					}
-					aria-label="Redo"
-					style={iconStyle}
-				>
-					<Icon
-						type={IconTypeEnum.ArrowForwardUp}
-						color={
-							!canGoForward || buttonsDisabled
-								? colorDisabled
-								: color
+					<TooltipWrapper
+						label={
+							hasPendingChanges
+								? "Accept or reject changes first"
+								: "Redo"
 						}
-					/>
-				</ActionIcon>
-			</TooltipWrapper>
+					>
+						<ActionIcon
+							onClick={goForward}
+							disabled={
+								!canGoForward || buttonsDisabled || executing
+							}
+							size={size}
+							variant={
+								!canGoForward || buttonsDisabled
+									? variantDisabled
+									: variant
+							}
+							aria-label="Redo"
+							style={iconStyle}
+						>
+							<Icon
+								type={IconTypeEnum.ArrowForwardUp}
+								color={
+									!canGoForward || buttonsDisabled
+										? colorDisabled
+										: color
+								}
+							/>
+						</ActionIcon>
+					</TooltipWrapper>
+				</>
+			)}
 
-			<Menu shadow="md" width={200} position="bottom-end">
-				<Menu.Target>
-					<ActionIcon
-						size={size}
-						variant={buttonsDisabled ? variantDisabled : variant}
-						aria-label="More options"
-						style={iconStyle}
-						disabled={buttonsDisabled}
-					>
-						<Icon
-							type={IconTypeEnum.DotsVertical}
-							color={buttonsDisabled ? colorDisabled : color}
-						/>
-					</ActionIcon>
-				</Menu.Target>
+			{enableMenu && (
+				<Menu shadow="md" width={200} position="bottom-end">
+					<Menu.Target>
+						<ActionIcon
+							size={size}
+							variant={
+								buttonsDisabled ? variantDisabled : variant
+							}
+							aria-label="More options"
+							style={iconStyle}
+							disabled={buttonsDisabled}
+						>
+							<Icon
+								type={IconTypeEnum.DotsVertical}
+								color={buttonsDisabled ? colorDisabled : color}
+							/>
+						</ActionIcon>
+					</Menu.Target>
 
-				<Menu.Dropdown>
-					<Menu.Item
-						onClick={resetParameters}
-						disabled={buttonsDisabled}
-					>
-						Reset to default values
-					</Menu.Item>
-					<Menu.Divider />
-					<Menu.Item
-						onClick={importParameters}
-						disabled={buttonsDisabled}
-					>
-						Import parameter values
-					</Menu.Item>
-					<Menu.Item
-						onClick={exportParameters}
-						disabled={buttonsDisabled}
-					>
-						Export parameter values
-					</Menu.Item>
-					<Menu.Divider />
-					<Menu.Item
-						onClick={onCreateModelState}
-						disabled={buttonsDisabled}
-					>
-						Create model state
-					</Menu.Item>
-					<Menu.Item
-						onClick={() => setIsImportDialogOpen(true)}
-						disabled={buttonsDisabled}
-					>
-						Import model state
-					</Menu.Item>
-				</Menu.Dropdown>
-			</Menu>
+					<Menu.Dropdown>
+						{enableResetButton && (
+							<Menu.Item
+								onClick={resetParameters}
+								disabled={buttonsDisabled}
+							>
+								Reset to default values
+							</Menu.Item>
+						)}
+						{enableImportExportButtons && (
+							<>
+								{enableResetButton && <Menu.Divider />}
+								<Menu.Item
+									onClick={importParameters}
+									disabled={buttonsDisabled}
+								>
+									Import parameter values
+								</Menu.Item>
+								<Menu.Item
+									onClick={exportParameters}
+									disabled={buttonsDisabled}
+								>
+									Export parameter values
+								</Menu.Item>
+							</>
+						)}
+						{enableModelStateButtons && (
+							<>
+								{(enableResetButton ||
+									enableImportExportButtons) && (
+									<Menu.Divider />
+								)}
+								<Menu.Item
+									onClick={onCreateModelState}
+									disabled={buttonsDisabled}
+								>
+									Create model state
+								</Menu.Item>
+								<Menu.Item
+									onClick={() => setIsImportDialogOpen(true)}
+									disabled={buttonsDisabled}
+								>
+									Import model state
+								</Menu.Item>
+							</>
+						)}
+					</Menu.Dropdown>
+				</Menu>
+			)}
 
-			<ImportModelStateDialog
-				opened={isImportDialogOpen}
-				onClose={() => setIsImportDialogOpen(false)}
-				namespace={namespace}
-			/>
+			{enableModelStateButtons && (
+				<ImportModelStateDialog
+					opened={isImportDialogOpen}
+					onClose={() => setIsImportDialogOpen(false)}
+					namespace={namespace}
+				/>
+			)}
 		</Box>
 	);
 }
