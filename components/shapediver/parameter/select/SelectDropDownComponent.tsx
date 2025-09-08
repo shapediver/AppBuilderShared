7d41983a66b13e9675dddb342996e02459bfb553
Dmitry Rumyantsev <lemtoup@gmail.com>
2025-09-08 15:34:51 +0200

    SS-8989: Add search ordering depends on selected value

--- components/shapediver/parameter/select/SelectDropDownComponent.tsx
+++ components/shapediver/parameter/select/SelectDropDownComponent.tsx
@@ -1,5 +1,5 @@
-import {Select} from "@mantine/core";
-import React from "react";
+import {ComboboxParsedItem, Select, defaultOptionsFilter} from "@mantine/core";
+import React, {useCallback} from "react";
 import {SelectComponentProps} from "./SelectComponent";
 
 /**
@@ -18,6 +18,51 @@ export default function SelectDropDownComponent(props: SelectComponentProps) {
 		limit,
 	} = props;
 
+	const onSearch = useCallback(
+		({
+			options,
+			search,
+			limit,
+		}: {
+			options: ComboboxParsedItem[];
+			search: string;
+			limit: number;
+		}) => {
+			const list = defaultOptionsFilter({options, search, limit});
+			if (!value) return list;
+
+			const foundIndex = list.findIndex((it: any) => {
+				if ("group" in it) {
+					return it.items?.some((item: any) => item.value === value);
+				}
+				return it.value === value;
+			});
+
+			if (foundIndex > 0) {
+				const foundItem = list.splice(foundIndex, 1)[0];
+
+				// If it's a group, move selected item to top within the group
+				if ("group" in foundItem) {
+					const selectedItemIndex = foundItem.items.findIndex(
+						(item: any) => item.value === value,
+					);
+					if (selectedItemIndex > 0) {
+						const selectedItem = foundItem.items.splice(
+							selectedItemIndex,
+							1,
+						)[0];
+						foundItem.items.unshift(selectedItem);
+					}
+				}
+
+				return [foundItem, ...list];
+			}
+
+			return list;
+		},
+		[value],
+	);
+
 	return (
 		<Select
 			value={value}
@@ -28,6 +73,7 @@ export default function SelectDropDownComponent(props: SelectComponentProps) {
 			inputContainer={inputContainer}
 			searchable={searchable}
 			limit={searchable ? (limit ?? 5) : undefined}
+			filter={searchable ? onSearch : undefined}
 		/>
 	);
 }
