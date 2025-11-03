c26cfd0a53a4f0212fe3a4e7966086ac445d7598
Dmitry Rumyantsev <lemtoup@gmail.com>
2025-11-03 13:28:42 +0100

    SS-9065: Switch animations

--- components/shapediver/appbuilder/widgets/AppBuilderStackUiWidget/AppBuilderStackUiWidgetAnimationWrapper.tsx
+++ components/shapediver/appbuilder/widgets/AppBuilderStackUiWidget/AppBuilderStackUiWidgetAnimationWrapper.tsx
@@ -16,33 +16,33 @@ export function AppBuilderStackUiWidgetAnimationWrapper({
 	const stackContext = useContext(AppBuilderStackContext);
 	const [showStack, setShowStack] = useState(isOpen);
 	const [showFallback, setShowFallback] = useState(!isOpen);
-	const [stackPosition, setStackPosition] = useState(isOpen ? "0" : "-100%");
+	const [stackPosition, setStackPosition] = useState(isOpen ? "0" : "100%");
 	const [fallbackPosition, setFallbackPosition] = useState(
-		isOpen ? "100%" : "0",
+		isOpen ? "-100%" : "0",
 	);
 
 	useEffect(() => {
 		if (isOpen) {
 			if (stackContext.isTransitioning) {
-				// Backward animation: fallback 100%→0, stack 0→-100%
+				// Backward (clicking "back"): left to right
 				setShowFallback(true);
 				setStackPosition("0");
-				setFallbackPosition("100%");
+				setFallbackPosition("-100%");
 				setTimeout(() => {
 					setFallbackPosition("0");
-					setStackPosition("-100%");
+					setStackPosition("100%");
 				}, 10);
 				setTimeout(() => {
 					setShowStack(false);
 				}, stackContext.animationDuration);
 			} else {
-				// Forward animation: fallback 0→100%, stack -100%→0
+				// Forward (clicking button): right to left
 				setShowStack(true);
 				setShowFallback(true);
-				setStackPosition("-100%");
+				setStackPosition("100%");
 				setFallbackPosition("0");
 				setTimeout(() => {
-					setFallbackPosition("100%");
+					setFallbackPosition("-100%");
 					setStackPosition("0");
 				}, 10);
 				setTimeout(() => {
