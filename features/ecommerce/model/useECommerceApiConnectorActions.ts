6cf1e2dcfc824d556dbca89a7af9057ddd5ebd63
Alexander Schiftner <alex@shapediver.com>
2026-05-27 20:51:37 +0200

    createModelState/importModelState via eCommerce API

--- features/ecommerce/model/useECommerceApiConnectorActions.ts
+++ features/ecommerce/model/useECommerceApiConnectorActions.ts
@@ -7,20 +7,29 @@ import {
 } from "@AppBuilderLib/features/ecommerce/config/ecommerceapi";
 import {validateUpdateParameterValuesData} from "@AppBuilderLib/features/ecommerce/config/ecommerceapitypecheck";
 import useAsync from "@AppBuilderLib/shared/lib/useAsync";
+import {useCreateModelState} from "@AppBuilderShared/features/model-state/model/useCreateModelState";
+import {useImportModelState} from "@AppBuilderShared/features/model-state/model/useImportModelState";
 import {useMemo} from "react";
 import {useShallow} from "zustand/react/shallow";
+
+interface Props {
+	namespace: string;
+}
 
 /**
  * Hook to set the actions for the e-commerce API connector.
  * As an example, this allows the e-commerce connector to trigger parameter
  * value updates in the App Builder app.
  */
-export function useECommerceApiConnectorActions() {
+export function useECommerceApiConnectorActions({namespace}: Props) {
 	const {batchParameterValueUpdate} = useShapeDiverStoreParameters(
 		useShallow((state) => ({
 			batchParameterValueUpdate: state.batchParameterValueUpdate,
 		})),
 	);
+
+	const {createModelState} = useCreateModelState({namespace});
+	const {importModelState} = useImportModelState({namespace});
 
 	const actions = useMemo(() => {
 		const updateParameterValues = async (
@@ -45,10 +54,12 @@ export function useECommerceApiConnectorActions() {
 
 		const actions: IECommerceApiConnectorActions = {
 			updateParameterValues,
+			createModelState,
+			importModelState,
 		};
 
 		return actions;
-	}, [batchParameterValueUpdate]);
+	}, [batchParameterValueUpdate, createModelState, importModelState]);
 
 	useAsync(async () => {
 		const api = await ECommerceApiSingleton;
