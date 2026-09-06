f6067390e2a28c6b55bc0b157ffcb84e9827d4d4
Alexander Schiftner <alex@shapediver.com>
2026-09-06 16:17:12 +0200

    remove unnecessary type import/export

--- features/appbuilder/config/appbuilderagent.ts
+++ features/appbuilder/config/appbuilderagent.ts
@@ -11,8 +11,6 @@ export enum GenericToolName {
 	GetMetric = "get_metric",
 }
 
-export type {DefaultListActionControlType} from "@AppBuilderLib/features/agent-tools/config/listActionControls";
-
 export type {
 	AskUserQuestionToolSettings,
 	GenericToolSettings,
